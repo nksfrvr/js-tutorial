ceeeabbc155553d7ebd292a264c88e67f620fb8d
nicu <nicolae.marin85@gmail.com>
2020-09-07 17:56:44 +0300

    first commit

---
+++ app.js
@@ -0,0 +1,50 @@
+
+//get elem by Id //
+
+//let addNew = document.getElementById('addBtn');
+//addNew.style.color = 'red';
+
+//Get elems by class name
+
+//let items = document.getElementsByClassName('list-group-item');
+//console.log(items);
+//for (var i = 0; i < items.length; i++) {
+  //items[i].style.backgroundColor = '#f4f4f4';
+//}
+
+//Get elems by tagname
+
+
+//let items = document.getElementsByTagName('li')
+//for (var i = 0; i < items.length; i++) {
+  //items[i].style.backgroundColor = 'aqua';
+//}
+//
+
+//QuerySelector//
+//let header = document.querySelector('.header'); //class name
+//header.style.borderBottom = 'solid 4px grey';
+//console.log(header);
+//
+//let input = document.querySelector('input');
+//input.value = 'hello world';
+//
+//let submit =document.querySelector('button[type="button"]');
+//submit.innerText = 'add';
+//
+////QuerySelectorAll
+//let items = document.querySelectorAll('.list-group-item');
+//console.log(items);
+
+//Odd an Even child selector
+
+//let odd = document.querySelectorAll('.list-group-item:nth-child(odd)');
+//
+//for (var i = 0; i < odd.length; i++) {
+//  odd[i].style.backgroundColor = 'red';
+//}
+
+//let even = document.querySelectorAll('.list-group-item:nth-child(even)');
+//for (var i = 0; i < even.length; i++) {
+//  even[i].style.backgroundColor ='coral';
+//}
